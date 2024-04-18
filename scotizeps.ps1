17d5e59b506a69a655b8b802b421d1be68d9434e
Scott John <29280467+swjpilot@users.noreply.github.com>
2024-04-18 08:03:30 -0400

    Bug Fixes script name

--- scotizeps.ps1
+++ scotizeps.ps1
@@ -1,4 +1,4 @@
-$env:scotizeps_ver = 29
+$env:scotizeps_ver = 30
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 Invoke-WebRequest -Uri https://s3.amazonaws.com/thejohnweb/scotizeps/versions.ps1 -Outfile "$HOME\Documents\WindowsPowerShell\versions.ps1"
 if (Test-Path "$HOME\Documents\WindowsPowerShell\versions.ps1") {
@@ -7,7 +7,7 @@ if (Test-Path "$HOME\Documents\WindowsPowerShell\versions.ps1") {
     Write-Host -ForegroundColor DarkMagenta "Online Scotize Version is $env:newscotizeps_ver"
     if ([INT]$env:newscotizeps_ver -gt [INT]$env:scotizeps_ver) {
         Write-Host -ForegroundColor DarkGreen "Scotize Version Upgrade Needed"
-        Invoke-WebRequest -Uri https://s3.amazonaws.com/thejohnweb/scotizeps/scotizeps.ps1 -Outfile "$HOME\Documents\WindowsPowerShell\scotize.ps1"
+        Invoke-WebRequest -Uri https://s3.amazonaws.com/thejohnweb/scotizeps/scotizeps.ps1 -Outfile "$HOME\Documents\WindowsPowerShell\scotizeps.ps1"
         Invoke-WebRequest -Uri https://s3.amazonaws.com/thejohnweb/scotizeps/Microsoft.PowerShell_profile.ps1 -Outfile "$HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
         $env:scotizeps_ver = $env:newscotizeps_ver
         refreshenv
--- versions.ps1
+++ versions.ps1
@@ -1,1 +1,1 @@
-$env:newscotizeps_ver = 29
+$env:newscotizeps_ver = 30
